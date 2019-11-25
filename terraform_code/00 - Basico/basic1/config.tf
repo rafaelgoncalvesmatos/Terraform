057e13a43bc74a40b023ff899ac6fa5e3bc276fd
rafael matos <rafael.matos@xpi.com.br>
2019-11-25 16:31:48 -0300

    opcoes

--- terraform_code/00 - Basico/basic1/config.tf
+++ terraform_code/00 - Basico/basic1/config.tf
@@ -6,10 +6,11 @@ resource "docker_container" "nginx-server" {
   name  = "nginx-server"
   image = "nginx"
 
+  # image = "${docker_image.nginx.latest}"
+
   ports {
     internal = 80
   }
-
   volumes {
     container_path = "/usr/share/nginx/html"
     host_path      = "/tmp"
